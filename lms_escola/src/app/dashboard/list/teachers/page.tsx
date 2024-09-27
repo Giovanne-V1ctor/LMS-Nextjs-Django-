1c65a5f29b688d9fd83a391a1311f2b372238f9a
giovanne <giovanne@batista.g12.br>
2024-09-27 15:57:04 -0300

    comit

--- lms_escola/src/app/dashboard/list/teachers/page.tsx
+++ lms_escola/src/app/dashboard/list/teachers/page.tsx
@@ -1,3 +1,5 @@
+import Pagination from "@/components/Pagination"
+import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
 import Image from "next/image"
 
@@ -9,21 +11,23 @@ const TeacherListPage = () => {
         <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch/>
-          <div className="">
-            <button>
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-[#FAE27C]">
               <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-[#FAE27C]">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-[#FAE27C]">
+              <Image src="/plus.png" alt="" width={14} height={14} />
             </button>
           </div>
         </div>
       </div>
       {/* LIST */}
-      <div className="">
-
-      </div>
+        <Table/>
       {/* PAGINNATION */}
-      <div className="">
-
-      </div>
+        <Pagination/>
     </div>
   )
 }
---
+++ lms_escola/src/components/Table.tsx
@@ -0,0 +1,9 @@
+const Table = () => {
+  return (
+    <table className='w-full mt-4'>
+        <thead></thead>
+    </table>
+  )
+}
+
+export default Table
