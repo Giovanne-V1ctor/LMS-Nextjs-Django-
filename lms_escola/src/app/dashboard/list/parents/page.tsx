61a6346f6d80821ca95cab100cbeaf8f351ee26d
giovanne <giovanne@batista.g12.br>
2024-09-29 14:43:18 -0300

    comit

--- lms_escola/src/app/dashboard/list/parents/page.tsx
+++ lms_escola/src/app/dashboard/list/parents/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal"
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
@@ -60,14 +61,11 @@ const ParentListPage = () => {
       </td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={"/dashboard/list/teachers/${item.id}"}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-[#C3EBFA]">
-              <Image src="/edit.png" alt="" width={16} height={16}/>
-            </button>
-          </Link>
-          {role === "admin" && (<button className="w-7 h-7 flex items-center justify-center rounded-full bg-[#CFCEFF]">
-              <Image src="/delete.png" alt="" width={16} height={16}/>
-            </button>
+          {role === "admin" && (
+            <>
+            <FormModal table="parent" type="update" data={item}/>
+            <FormModal table="parent" type="delete" id={item.id}/>
+            </>
           )}
         </div>
       </td>
@@ -89,9 +87,10 @@ const ParentListPage = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-                <button className="w-8 h-8 flex items-center justify-center rounded-full bg-[#FAE27C]">
-              <Image src="/plus.png" alt="" width={14} height={14} />
-            </button>
+                //<button className="w-8 h-8 flex items-center justify-center rounded-full bg-[#FAE27C]">
+                //  <Image src="/plus.png" alt="" width={14} height={14} />
+                //</button>
+                <FormModal table="parent" type="create"/>
             )}
           </div>
         </div>
--- lms_escola/src/components/FormModal.tsx
+++ lms_escola/src/components/FormModal.tsx
@@ -1,7 +1,19 @@
-"use client"
+"use client";
 
-const FormModal = ({table, type, data, id}:{
-    table: "teacher" | "student" | "parent"
+import Image from "next/image";
+import { useState } from "react";
+import TeacherForm from "./forms/TeacherForm";
+
+const FormModal = ({
+  table,
+  type,
+  data,
+  id,
+}: {
+  table:
+    | "teacher"
+    | "student"
+    | "parent"
     | "subject"
     | "class"
     | "lesson"
@@ -11,16 +23,47 @@ const FormModal = ({table, type, data, id}:{
     | "attendance"
     | "event"
     | "announcement";
-    type: "create" | "update" | "delete";
-    data?:any;
-    id?:number; 
+  type: "create" | "update" | "delete";
+  data?: any;
+  id?: number;
 }) => {
+  const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
+  const bgColor =
+    type === "create"
+      ? "bg-[#FAE27C]"
+      : type === "update"
+      ? "bg-[#C3EBFA]"
+      : "bg-[#CFCEFF]";
+  const [open, setOpen] = useState(false);
 
-    const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
-    const bgColor = type === "create" ? "bg-[#FAE27C]" : type === "update" ? "bg-[#C3EBFA]" : "bg-[#CFCEFF]";
-        return  <>
-        <button className=""/>
-        </>
-}
+  const Form = ()=>{
+    return type === "delete" && id ? (
+    <form action="" className="p-4 flex flex-col gap-4">
+        <span className="text-center font-medium">All data will be lost. Are you sure you want to delete this {table}?</span>
+        <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
+    </form>
+    ) : (
+       <TeacherForm type="create"/>
+    );
+  }
+  return (
+    <>
+      <button
+        className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+        onClick={() => setOpen(true)}
+      >
+        <Image src={`/${type}.png`} alt="" width={16} height={16} />
+      </button>
+      {open && <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+            <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+                <Form/>
+                <div className="absolute top-4 right-4 cursor-pointer" onClick={()=>setOpen(false)}>
+                    <Image src="/close.png" alt="" width={14} height={14}/>
+                </div>
+            </div>
+        </div>}
+    </>
+  );
+};
 
-export default FormModal
+export default FormModal;
